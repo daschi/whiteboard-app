9571de50ec117bbb696e2523f0a10cbe3366f2b9
daschi <daschi87@gmail.com>
2015-08-24 10:06:21 -0700

    Add more notes to the front end index js file. Add the video information to try and emit videos

--- whiteboard-app/public/javascripts/index.js
+++ whiteboard-app/public/javascripts/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	// Capture the DOM elements
+// Capture the DOM elements
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
 	var $canvas = $("#canvas");
@@ -7,82 +7,83 @@ $(document).ready(function() {
 	var lineWidth = 10;
 	var drawing;
 
-	// Set the client-side connection 
+// Set the client-side connection 
 	var socket = io.connect('http://localhost:3000');
 
-	// Listen for draw messages from server
+// Listen for draw messages from server
 	socket.on('draw', function (data) {
 		console.log("on draw from the client")
 		draw(data);
 	});
 
 // Begin draw	
-$canvas.mousedown(function(event) {
-	drawing = true;
-	
-	currentX = event.pageX - this.offsetLeft;
-	currentY = event.pageY - this.offsetTop;
-
-});
-
-// End draw
-$canvas.mouseup(function(event) {
-	drawing = false;
-})
-
-$canvas.mouseleave(function(event) {
-	drawing = false
-})
-
-// Draw
-$canvas.mousemove(function(event){
-
-	if (drawing) {
-		prevX = currentX;
-		prevY = currentY;
+	$canvas.mousedown(function(event) {
+		drawing = true;
+		
 		currentX = event.pageX - this.offsetLeft;
 		currentY = event.pageY - this.offsetTop;
 
-		socket.emit('draw', {
-				x: currentX,
-				y: currentY,
-				prevX: prevX,
-				prevY: prevY,
-				strokeStyle: strokeStyle,
-				lineWidth: lineWidth,
-		});
+	});
+
+// End draw
+	$canvas.mouseup(function(event) {
+		drawing = false;
+	})
+
+	$canvas.mouseleave(function(event) {
+		drawing = false
+	})
+
+// Pick color and line width
+	$(".color").on('click', function(event) {
+		event.preventDefault();
+		strokeStyle = $(this).css( "background-color" )
+	})
+
+	$(".line-width").on('click', function(event) {
+		event.preventDefault();
+		lineWidth = $(this).attr('id')
+	})
+
+// Set x/y coordinates and send data to server
+	$canvas.mousemove(function(event){
+
+		if (drawing) {
+			prevX = currentX;
+			prevY = currentY;
+			currentX = event.pageX - this.offsetLeft;
+			currentY = event.pageY - this.offsetTop;
+
+			socket.emit('draw', {
+					x: currentX,
+					y: currentY,
+					prevX: prevX,
+					prevY: prevY,
+					strokeStyle: strokeStyle,
+					lineWidth: lineWidth,
+			});
+		}
+
+	});
+
+// Draw using the x/y coordinates
+	function draw(data) {
+		ctx.beginPath();
+		ctx.strokeStyle = data.strokeStyle;
+		ctx.lineJoin = "round";
+		ctx.lineWidth = data.lineWidth;
+		ctx.moveTo(data.prevX, data.prevY);
+		ctx.lineTo(data.x, data.y);
+		ctx.closePath();
+		ctx.stroke();
 	}
-
-});
-
-$(".color").on('click', function(event) {
-	event.preventDefault();
-	strokeStyle = $(this).css( "background-color" )
-})
-
-$(".line-width").on('click', function(event) {
-	event.preventDefault();
-	lineWidth = $(this).attr('id')
-})
-
-function draw(data) {
-	ctx.beginPath();
-	ctx.strokeStyle = data.strokeStyle;
-	ctx.lineJoin = "round";
-	ctx.lineWidth = data.lineWidth;
-	ctx.moveTo(data.prevX, data.prevY);
-	ctx.lineTo(data.x, data.y);
-	ctx.closePath();
-	ctx.stroke();
-}
 	
-// Set username
-var username;
-$('form.username').submit(function() {
-	username = $('#name').val();
-	debugger
-	$(this).hide();
-})
+// Set username -- Need to fix this
+	var username;
+	$('form.username').submit(function() {
+		username = $('#name').val();
+		$(this).hide();
+	})
 
 // Send chat messages
 	$('form.send-chat').submit(function() {
@@ -96,29 +97,29 @@ $('form.username').submit(function() {
 		return false;
 	});
 
-	// Listen for chat messages
+// Listen for chat messages
 	socket.on('chat message', function(message) {
 		$('#chatbox').append("<p>" + message.name + ": " + message.text + "</p>");
 	});
 
-});
+// Set video element to variable and attempt to emit it - Need to fix this
+	var video = document.getElementById('video-id')
 
-function addChatTyping(data){
+	$('video').on('timeupdate', function(event){
+		if (!video.ended){
+			for (var i = 0; i <= this.duration; i++){
+				socket.emit('timeupdate', (this.currentTime));
+				console.log("EMIT client video, current time is: " + this.currentTime);
+				this.currentTime += i
+			}
+		}
+	});
 
-}
+	socket.on('timeupdate', function (time){
+		video.currentTime = time 
+		console.log("socket ON client video, current time is: " + time);
+	});
 
-function removeChatTyping(data){
-	
-}
-
-
-// GOALS FOR SUNDAY:
-// USER LOGIN
-// TOGGLE COLORS ON WHITEBOARD
-// READ MORE ABOUT SOCKETS
-// TRANSFER FROM RAILS?
-
-
-
+}); // end document ready
 
 
